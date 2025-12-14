601e18f19ed8dcef4d8f259517c5011b8e8eef14
Okibob - LAURET Timéo <130897927+0kibob@users.noreply.github.com>
2025-12-14 21:30:25 +0100

    Add basic structure

--- web/src/main.py
+++ web/src/main.py
@@ -7,9 +7,28 @@ from flask import session
 
 app: Flask = Flask(__name__)
 
+# GLOBAL
+@app.context_processor
+def inject_site_name():
+    return {"site_name": "SampleCan"}
+
+# ROUTE
 @app.route('/')
 def home() -> str:
-    return render_template('test.html')
+    return render_template('pages/home.html')
 
-if not __name__ == "__main__": quit()
-app.run(debug=True, host='0.0.0.0', port=5000)
+# ERROR
+def error_handler(error_code: int, error_name: str, error_description: str):
+    error_data: list = [error_code, error_name, error_description]
+    return render_template('special/error.html', error_info=error_data)
+
+@app.errorhandler(404)
+def error_404(error):
+    return error_handler(404, "404 Not Found", "Oops... The page you are looking for does not exist. UwU")
+
+@app.errorhandler(405)
+def error_401(e):
+    return error_handler(405, "401 Method Not Allowed", "The method is not allowed for the requested URL.")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
