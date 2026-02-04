4b34adc64c4b010d85985d38eead5d3443728e77
LAURET Timéo <130897927+0kibob@users.noreply.github.com>
2026-02-04 15:46:19 +0100

    my pc broke

--- web/src/main.py
+++ web/src/main.py
@@ -2,9 +2,6 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import abort
-from flask import redirect
-from flask import url_for
-from flask import session
 
 app: Flask = Flask(__name__)
 
